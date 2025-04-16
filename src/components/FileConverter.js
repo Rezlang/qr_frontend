c914ebd56d55554547365f6c107b6662af5a62ab
Luucat <mrdrmedic45@gmail.com>
2025-04-16 16:54:02 -0400

    Docx -> HTML, txt | Restructure to support modular converter

--- src/components/FileConverter.js
+++ src/components/FileConverter.js
@@ -1,8 +1,9 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect, useCallback } from 'react';
 import { Container, Box, Button, Typography, MenuItem, Select, FormControl, InputLabel } from '@mui/material';
 import { styled } from '@mui/system';
 import { FFmpeg } from '@ffmpeg/ffmpeg';
 import { fetchFile, toBlobURL } from '@ffmpeg/util';
+import mammoth from 'mammoth';
 
 const UploadBox = styled(Box)({
   border: '2px dashed #ccc',
@@ -19,48 +20,140 @@ const ResultBox = styled(Box)({
   marginBottom: '20px',
 });
 
-const FileConverter = () => {
+const ffmpegMap = {
+  'png': ['jpg', 'jpeg', 'gif', 'webp'],
+  'jpg': ['png', 'jpeg', 'gif', 'webp'],
+  'jpeg': ['png', 'jpg', 'gif', 'webp'],
+  'gif': ['png', 'jpg', 'jpeg', 'webp'],
+  'webp': ['png', 'jpg', 'jpeg', 'gif'],
+  'mp3' : ['wav', 'ogg'],
+  'wav' : ['mp3', 'ogg'],
+  'ogg' : ['mp3', 'wav'],
+  'mp4' : ['mkv', 'avi', 'mov'],
+  'mkv' : ['mp4', 'avi', 'mov'],
+  'avi' : ['mp4', 'mkv', 'mov'],
+  'mov' : ['mp4', 'mkv', 'avi'],
+}
+
+const textMap = {
+  'docx': ['txt', 'html'],
+}
+
+const mimeTypes = {
+  'png': 'image/png',
+  'jpg': 'image/jpeg',
+  'jpeg': 'image/jpeg',
+  'gif': 'image/gif',
+  'webp': 'image/webp',
+  'mp3': 'audio/mpeg',
+  'wav': 'audio/wav',
+  'ogg': 'audio/ogg',
+  'mp4': 'video/mp4',
+  'mkv': 'video/x-matroska',
+  'avi': 'video/x-msvideo',
+  'mov': 'video/quicktime',
+  'txt': 'text/plain',
+  'html': 'text/html',
+  'docx': 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+  'pdf': 'application/pdf',
+}
+
+export default function FileConverter() {
   const [file, setFile] = useState(null);
-  const [uploadError, setUploadError] = useState(null);
   const [result, setResult] = useState(null);
-  const [inputType, setInputType] = useState(null);
-  const [outputType, setOutputType] = useState(null);
+  const [uploadError, setUploadError] = useState('');
+  const [inputType, setInputType] = useState('');
+  const [outputType, setOutputType] = useState('');
+
+  const mapRef = useRef({});
   const ffmpegRef = useRef(new FFmpeg());
-  const messageRef = useRef(null);
-
-  const ffmpegMap = {
-    'png': ['jpg', 'jpeg', 'gif', 'webp'],
-    'jpg': ['png', 'jpeg', 'gif', 'webp'],
-    'jpeg': ['png', 'jpg', 'gif', 'webp'],
-    'gif': ['png', 'jpg', 'jpeg', 'webp'],
-    'webp': ['png', 'jpg', 'jpeg', 'gif'],
-    'mp3' : ['wav', 'ogg'],
-    'wav' : ['mp3', 'ogg'],
-    'ogg' : ['mp3', 'wav'],
-    'mp4' : ['mkv', 'avi', 'mov'],
-    'mkv' : ['mp4', 'avi', 'mov'],
-    'avi' : ['mp4', 'mkv', 'mov'],
-    'mov' : ['mp4', 'mkv', 'avi'],
-  }
+  const converterRef = useRef(null);
+  const messageRef = useRef();
 
 
-  const mimeTypes = {
-    'png': 'image/png',
-    'jpg': 'image/jpeg',
-    'jpeg': 'image/jpeg',
-    'gif': 'image/gif',
-    'webp': 'image/webp',
-    'mp3': 'audio/mpeg',
-    'wav': 'audio/wav',
-    'ogg': 'audio/ogg',
-    'mp4': 'video/mp4',
-    'mkv': 'video/x-matroska',
-    'avi': 'video/x-msvideo',
-    'mov': 'video/quicktime',
-  }
+  const getOutputFilename = useCallback(() => {
+    const fileNameNoExt = file.name.split('.')[0];
+    return `${fileNameNoExt}.${outputType}`;
+  }, [file, outputType]);
+
+  useEffect(() => {
+    if (!inputType) return;
+
+    const ffmpegConvert = async () => {
+      await load();
+      
+      const ffmpeg = ffmpegRef.current
+      const outputFile = getOutputFilename();
+  
+      await ffmpeg.writeFile(`${file.name}`, await fetchFile(file));
+      await ffmpeg.exec(['-i', `${file.name}`, outputFile]);
+      const data = await ffmpeg.readFile(outputFile);
+      setResult(data);
+    }
+    const selectTextConvert = (inputFile) => {
+      switch (inputFile) {
+        case 'docx':
+          return mammothConvert;
+        default:
+          return null;
+      }
+    }
+
+    const docxToText = async (reader) => {
+      try {
+        const res = await mammoth.extractRawText({arrayBuffer: reader.result});
+        setResult(res.value);
+      } catch (err) {
+        console.error("Error converting file:", err);
+      }
+    }
+    const docxToHTML = async (reader) => {
+      try {
+        const res = await mammoth.convertToHtml({arrayBuffer: reader.result});
+        setResult(res.value);
+      } catch (err) {
+        console.error("Error converting file:", err);
+      }
+    }
+
+    const mammothConvert = async () => {
+      const reader = new FileReader();
+      reader.readAsArrayBuffer(file);
+      reader.onload = async () => {
+        switch (outputType) {
+          case 'html':
+            await docxToHTML(reader);
+            break;
+          case 'txt':
+            await docxToText(reader);
+            break;
+          default:
+        } 
+      }
+    }
+
+    if (ffmpegMap[inputType]) {
+      mapRef.current = ffmpegMap;
+      converterRef.current = ffmpegConvert;
+      setUploadError('');
+    } else if (textMap[inputType]) {
+      mapRef.current = textMap;
+      converterRef.current = selectTextConvert(inputType);
+      setUploadError('');
+    } else {
+      mapRef.current = {};
+      converterRef.current = null;
+      setUploadError('Invalid file type');
+      setFile(null);
+      return;
+    }
+
+    // default to first available output for that type
+    setOutputType(outputType ? outputType : mapRef.current[inputType][0]);
+  }, [inputType, getOutputFilename, file, outputType]);
 
   function extToMime(ext) {
-    return mimeTypes[ext] || null;
+    return mimeTypes[ext] || 'application/octet-stream';
   } 
 
   const formatFileSize = (size) => {
@@ -71,22 +164,11 @@ const FileConverter = () => {
   };
 
   const handleFileChange = (event) => {
-    if (event.target.files && event.target.files[0]) {
-      const file = event.target.files[0];
-      // Get the type of the file
-      const fileType = file.name.split('.')[1].toLowerCase();
-      if (fileType in ffmpegMap) {
-        setUploadError(null);
-        setInputType(fileType);
-        setOutputType(ffmpegMap[fileType][0]);
-        setFile(file);
-      }
-      else {
-        setUploadError('Invalid file type');
-        setFile(null);
-        return;
-      }
-    }
+    const f = event.target.files?.[0];
+    if (!f) return;
+    const extension = f.name.split('.').pop().toLowerCase();
+    setInputType(extension);
+    setFile(f);
   };
 
   const load = async () => {
@@ -107,35 +189,27 @@ const FileConverter = () => {
 }
 
   const handleConvert = async () => {
-      setResult(null);
-      console.log("Attempting conversion")
-    const fileNameNoExt = file.name.split('.')[0];
-    const outputFile = `${fileNameNoExt}.${outputType}`;
-    if (file) {
-      await load();
-      const ffmpeg = ffmpegRef.current
-      await ffmpeg.writeFile(`${file.name}`, await fetchFile(file));
-      await ffmpeg.exec(['-i', `${file.name}`, outputFile]);
-      const data = await ffmpeg.readFile(outputFile);
-      setResult(data);
-      console.log("Completed Conversion");
+    setResult(null);
+    console.log("Attempting conversion");
+    
+    if (file && converterRef.current) {
+      await converterRef.current();      
     }
   };
 
   const handleDownload = () => {
-    console.log("attempting download");
-    if (result) {
-      const element = document.createElement('a');
-      const fileBlob = new Blob([result.buffer], { type: extToMime(outputType) });
-      element.href = URL.createObjectURL(fileBlob);
-      element.download = `converted-file.${outputType}`;
-      document.body.appendChild(element);
-      element.click();
-    }
-  };
-
-  const handleOutputTypeChange = (event) => {
-    setOutputType(event.target.value);
+    console.log("Attempting download");
+    if (!result) return;
+    const element = document.createElement('a');
+    const fileBlob = 
+      result.buffer != null
+        ? new Blob([result.buffer], { type: extToMime(outputType) })
+        : new Blob([result], { type: extToMime(outputType) });
+    element.href = URL.createObjectURL(fileBlob);
+    element.download = getOutputFilename();
+    document.body.appendChild(element);
+    element.click();
+    element.remove();
   };
 
   return (
@@ -146,7 +220,7 @@ const FileConverter = () => {
       <Typography variant="body1" gutterBottom>
         Upload a file to convert it to a different format. Supports common image, audio, and video formats.
       </Typography>
-          <UploadBox>
+      <UploadBox>
         <input
           type="file"
           onChange={handleFileChange}
@@ -159,9 +233,9 @@ const FileConverter = () => {
           </Button>
         </label>
         {file && 
-        <Typography variant="body1">
-          {file.name} ({formatFileSize(file.size)})
-        </Typography>
+          <Typography variant="body1">
+            {file.name} ({formatFileSize(file.size)})
+          </Typography>
         }
         {uploadError && ( // Display error message if uploadError is set
           <Typography variant="body2" color="error" style={{ marginTop: '10px' }}>
@@ -173,19 +247,18 @@ const FileConverter = () => {
         variant="contained"
         color="primary"
         onClick={handleConvert}
-        disabled={!file}
+        disabled={!file || !!uploadError}
       >
         Convert
       </Button>
-      {inputType && (
-        <FormControl  style={{ marginLeft: '10px', minWidth: '100px' }}>
-          <InputLabel  id="output-type-label">Output Type</InputLabel>
+      {inputType && Array.isArray(mapRef.current[inputType]) && (
+        <FormControl style={{ marginLeft: '10px', minWidth: '100px' }}>
+          <InputLabel>Output Type</InputLabel>
           <Select
-            labelId="output-type-label"
             value={outputType}
-            onChange={handleOutputTypeChange}
+            onChange={(e) => setOutputType(e.target.value)}
           >
-            {ffmpegMap[inputType].map((type) => (
+            {mapRef.current[inputType].map((type) => (
               <MenuItem key={type} value={type}>
                 {type.toUpperCase()}
               </MenuItem>
@@ -196,7 +269,7 @@ const FileConverter = () => {
       <p ref={messageRef}></p>
       {result && (
         <ResultBox>
-          <Typography variant="body1">{`converted-file.${outputType} (${formatFileSize(result.length)})`}</Typography>
+          <Typography variant="body1">{`${getOutputFilename()} (${formatFileSize(result.length)})`}</Typography>
           <Button variant="contained" color="secondary" onClick={handleDownload}>
             Download Result
           </Button>
@@ -205,5 +278,3 @@ const FileConverter = () => {
     </Container>
   );
 };
-
-export default FileConverter;
